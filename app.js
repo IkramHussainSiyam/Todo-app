ada06b0e62e110db737987db15ec665bd63504a1
IkramHussainSiyam <ikramhussaindev404@gmail.com>
2023-08-16 19:39:58 +0600

    updated v2.1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const addTodo = document.querySelector("#addTodo");
 const taskInput = document.querySelector(".taskInput");
 const todolist = document.querySelector(".todolist");
+const deleteAll = document.querySelector("#deleteAll");
+const totalTodo = document.querySelector("#totalTodo");
+const countCompleted = document.querySelector("#countCompleted");
 
 addTodo.addEventListener("click", function () {
   let taskInputValue = taskInput.value;
@@ -20,14 +23,31 @@ addTodo.addEventListener("click", function () {
     todolist.appendChild(li);
   }
   taskInput.value = "";
+  totalTodo.innerText = todolist.childElementCount;
+  updateCompletedCount();
 });
 
 todolist.addEventListener("click", function (e) {
   if (e.target.className.includes("todoDone")) {
     e.target.parentElement.classList.toggle("checked");
+    updateCompletedCount();
   } else if (e.target.className.includes("task")) {
     e.target.previousElementSibling.classList.toggle("checked");
+    updateCompletedCount();
   } else if (e.target.className.includes("deleteTodo")) {
     e.target.parentElement.remove();
+    totalTodo.innerText = todolist.childElementCount;
+    updateCompletedCount();
   }
+});
+
+function updateCompletedCount() {
+  const count = document.querySelectorAll(".todolist .checked").length;
+  countCompleted.innerText = count;
+}
+
+deleteAll.addEventListener("click", function () {
+  todolist.innerHTML = "";
+  totalTodo.innerText = todolist.childElementCount;
+  updateCompletedCount();
 });
